a8295e09d52a41ca95c288c11f02f7b4a0342515
erinecampbell <eec35@duke.edu>
2023-05-14 22:07:32 -0400

    pulling gdoc edits and trying to add reliability. currently doesn't knit

--- input_quality_preprocessing.R
+++ input_quality_preprocessing.R
@@ -12,7 +12,7 @@ library(profileR)
 library(lubridate)
 library(morphemepiece)
 library(udpipe)
-
+library(irr)
 
 ## demographics
 VI_matches_demo <- read.csv("data/VI_matches_demo.csv") 
@@ -75,8 +75,12 @@ VITD_LENA_words <- VITD_LENA_utterances_split %>%
 utterances_only<-VITD_transcripts %>% 
   filter(speaker!="EE1", speaker!="CHI") %>% # remove CHI utts and electronic noise
   filter(!utterance==c("xxx.")) %>% #remove unintelligible utterances
-  mutate(utterance = str_remove_all(utterance, pattern = "[[:punct:]]"),# remove punctuation from utterances
-         utt_num = 1:nrow(.)) 
+  
+  mutate(utterance = str_replace_all(utterance, "<\\w+>", ""),
+         utterance = str_replace_all(utterance, "=!\\w+\\s*", ""),
+         utterance = str_replace_all(utterance, "[[:punct:]&&[^']]", ""),# remove punctuation from utterances
+         utt_num = 1:nrow(.)) %>%
+  filter(!grepl("utt@|inq@", speaker))
 
 
 # quantity ----
@@ -138,8 +142,6 @@ TTR_calculations <- VITD_LENA_words %>%
 
 
 ## MLU ----
-### get the morpheme counts for VI ----
-
 tokenized_VITD_transcripts <- morphemepiece_tokenize(utterances_only$utterance, vocab = morphemepiece_vocab(),
                                                      lookup = morphemepiece_lookup(),
                                                      unk_token = "[UNK]",
@@ -158,10 +160,36 @@ simple_morpheme_counts <- tokenized_VITD_transcripts_with_counts %>%
   mutate(group=as.factor(str_sub(VIHI_ID,1,2)))
 
 MLUs <- simple_morpheme_counts %>% 
+  filter(!is.na(utterance)) %>%
   group_by(group, VIHI_ID) %>%
   summarise(MLU= mean(morphemecount)) %>%
   left_join((VI_matches_demo %>% dplyr::select(VIHI_ID,pair)), by="VIHI_ID")
 write.csv(MLUs,"data/LENA/Transcripts/Derived/MLUs.csv")
+
+random_MLU_subset <- simple_morpheme_counts%>%
+    filter(!is.na(morphemecount) &!is.na(utterance)) %>%
+  sample_n(round(nrow(simple_morpheme_counts%>%filter(!is.na(morphemecount) &!is.na(utterance)))*.1))
+
+# secret_random_MLU_subset <- random_MLU_subset %>%
+#   dplyr::select(utt_num, speaker, xds,utterance) %>%
+#   mutate(manual_morpheme_count = "NA")
+# # # Export the random subset to a CSV file for manual coding in Excel
+# write_csv(secret_random_MLU_subset, "data/LENA/Transcripts/Derived/secret_random_MLU_subset.csv")
+
+manually_coded_MLU_subset <- read.csv("data/LENA/Transcripts/Derived/manually_coded_MLU_subset.csv")
+
+
+# Calculate agreement between the manual coding and original data
+MLU_subset_for_agreement <- left_join((manually_coded_MLU_subset %>% distinct(utterance,utt_num,.keep_all = TRUE)),
+                                           random_MLU_subset, 
+                                           by = c("utterance","utt_num")) %>%
+  dplyr::select(utterance,morphemecount, manual_morpheme_count) %>%
+  filter(!is.na(manual_morpheme_count)) %>%
+  mutate(morpheme_diff = morphemecount-manual_morpheme_count,
+         abs_morpheme_diff = abs(morphemecount-manual_morpheme_count))
+
+# Calculate Intraclass Correlation
+MLU_agreement <- icc(MLU_subset_for_agreement)
 
 
 # conceptual quality ----
@@ -169,8 +197,9 @@ write.csv(MLUs,"data/LENA/Transcripts/Derived/MLUs.csv")
 sensory_props_wide <- VITD_LENA_words %>% 
   anti_join(stop_words, by= c("Word" = "word")) %>%
   filter(!is.na(Auditory.mean))  %>% # filter out words that don't have a perceptual rating
-  mutate(Modality = case_when(Exclusivity.perceptual <= .5 ~ "Multimodal",
-                              Max_strength.perceptual<3.5 ~ "Amodal",
+  mutate(Modality = case_when(Max_strength.perceptual<3.5 ~ "Amodal",
+    Exclusivity.perceptual <= .5 ~ "Multimodal",
+
                               Max_strength.perceptual>4  &  Exclusivity.perceptual <= .3 ~ Dominant.perceptual,
                               TRUE~Dominant.perceptual)) %>%
   group_by(group,VIHI_ID) %>% 
@@ -193,6 +222,38 @@ sensory_props <- sensory_props_wide%>%
 
 write.csv(sensory_props,"data/LENA/Transcripts/Derived/sensory_props.csv")
 
+
+# random_modality_subset <- VITD_LENA_words%>%
+#   filter(!is.na(Visual.mean)) %>%
+#   sample_n(round((0.1 * nrow(VITD_LENA_words%>%
+#                                filter(!is.na(Visual.mean)))))) %>%
+#   mutate(Modality = case_when(Exclusivity.perceptual <= .5 ~ "Multimodal",
+#                               Max_strength.perceptual<3.5 ~ "Amodal",
+#                               Max_strength.perceptual>4  &  Exclusivity.perceptual <= .3 ~ Dominant.perceptual,
+#                               TRUE~Dominant.perceptual)) 
+random_modality_subset <- read_csv("data/LENA/Transcripts/Derived/random_modality_subset.csv")
+
+# secret_random_modality_subset <- random_modality_subset %>%
+#   dplyr::select(Word) %>%
+#   mutate(manual_modality = "NA")
+# # Export the random subset to a CSV file for manual coding in Excel
+# write_csv(secret_random_modality_subset, "data/LENA/Transcripts/Derived/secret_random_modality_subset.csv")
+# Once manual coding is done in Excel, read the manually coded CSV file back into R
+manually_coded_modality_subset <- read_csv("data/LENA/Transcripts/Derived/manually_coded_modality_sample.csv")
+
+# Calculate agreement between the manual coding and original data
+modality_subset_for_agreement <- left_join((random_modality_subset %>% distinct(Word,.keep_all = TRUE)),
+                                           manually_coded_modality_subset, 
+                                                 by = c("Word")) %>%
+  dplyr::select(Word, Modality, manual_modality) 
+
+# Calculate Percent Agreement
+modality_agreement <- sum(modality_subset_for_agreement$Modality == modality_subset_for_agreement$manual_modality) / nrow(modality_subset_for_agreement)
+visual_agreement <- sum((modality_subset_for_agreement%>% filter(manual_modality == "Visual"))$Modality == (modality_subset_for_agreement%>% filter(manual_modality == "Visual"))$manual_modality) / nrow(modality_subset_for_agreement %>% filter(manual_modality == "Visual"))
+  
+
+
+
 ## tense/displacement----
 udpipe_english <- udpipe_download_model(language = "english")
 udmodel_english <- udpipe_load_model(file = udpipe_english$file_model)
@@ -214,6 +275,31 @@ verbs_only <- annotated_utterances %>%
                                  grepl('Mood=Ind',feats) & grepl('Tense=Pres',feats) |grepl('VerbForm=Ger',feats) ~ "present",
                                  TRUE ~ "uncategorized")) 
 write.csv(verbs_only, "data/LENA/Transcripts/Derived/verbs_only.csv")
+
+# Randomly select a subset of the utterances for manual coding
+# random_displacement_subset <- verbs_only %>%
+#   sample_n(round((0.1 * nrow(verbs_only)))) 
+# secret_random_displacement_subset <- random_displacement_subset %>%
+#   dplyr::select(-doc_id, -upos, -xpos, -feats, -temporality) %>%
+#   mutate(manual_temporality = "NA")
+# Export the random subset to a CSV file for manual coding in Excel
+# write_csv(secret_random_displacement_subset, "data/LENA/Transcripts/Derived/secret_random_displacement_subset.csv")
+# Once manual coding is done in Excel, read the manually coded CSV file back into R
+random_displacement_subset <- read_csv("data/LENA/Transcripts/Derived/random_displacement_subset.csv")
+
+manually_coded_displacement_subset <- read_csv("data/LENA/Transcripts/Derived/manually_coded_displacement_subset.csv")
+# Calculate agreement between the manual coding and original data
+displacement_subset_for_agreement <- left_join(random_displacement_subset,
+(manually_coded_displacement_subset %>% mutate(token_id=as.character(token_id),
+                                               head_token_id=as.character(head_token_id))), 
+by = c("paragraph_id", "sentence_id", "sentence","token_id", "token", "lemma","head_token_id","dep_rel","deps","misc")) %>%
+  dplyr::select(doc_id, paragraph_id, sentence_id, sentence, temporality, manual_temporality) 
+
+# Calculate Percent Agreement
+displacement_agreement <- sum(displacement_subset_for_agreement$temporality == displacement_subset_for_agreement$manual_temporality, na.rm=TRUE) / nrow(displacement_subset_for_agreement %>% filter(!is.na(manual_temporality)))
+displaced_agreement <- sum((displacement_subset_for_agreement %>% filter(manual_temporality == "displaced"))$temporality == (displacement_subset_for_agreement%>% filter(manual_temporality == "displaced"))$manual_temporality) / nrow(displacement_subset_for_agreement %>% filter(manual_temporality == "displaced"))
+
+
 temporality_props_wide <- verbs_only %>% 
   dplyr::rename(VIHI_ID = doc_id) %>%
   group_by(VIHI_ID) %>%
