c6d48ad4b0c5d9a22940045fe10d9a308edde8e0
erinecampbell <eec35@duke.edu>
2024-01-10 12:11:57 -0500

    commenting code for code review

--- manuscript/input_quality_preprocessing.R
+++ manuscript/input_quality_preprocessing.R
@@ -1,15 +1,13 @@
 # Input Quality Preprocessing
 
 ## demographics
-VI_matches_demo <- read.csv("./data/Demographics/VI_matches_demo.csv")
+VI_matches_demo <- read_csv("../data/Demographics/VI_matches_demo.csv")
 
 lancaster_norms <-
-  read.csv("data/Norms/Lancaster_sensorimotor_norms_for_39707_words.csv") %>%
-  mutate(Word = tolower(Word))
-CBOI_norms <- read.csv("data/Norms/CBOI_mean_sd.csv") %>%
-  mutate(Word = tolower(Word))
+  read_csv("../data/Norms/Lancaster_sensorimotor_norms_for_39707_words.csv") %>%
+  mutate(Word = tolower(Word)) # make the contents of the Word column lowercase (to align with our transcription style)
 
-### find + list all LENA transcripts on pn-opus
+### list of TD matches
 TD_matches <-
   c(
     "TD_436_678",
@@ -22,7 +20,6 @@ TD_matches <-
     "TD_463_254",
     "TD_464_188",
     "TD_465_541",
-    # "TD_467_433",
     "TD_472_829",
     "TD_473_844",
     "TD_474_966",
@@ -30,38 +27,36 @@ TD_matches <-
     "TD_477_217"
   )
 
-
-LENA_counts <-
-  read.csv("data/LENA/Automated/VIHI_its_details.csv") %>%
-  filter(match_type == "VI" |
-           VIHI_ID %in% TD_matches) %>%
-  mutate(
-    AWC_stand = AWC / (total_time_dur / (30 * 60)),
-    CTC_stand = CTC / (total_time_dur / (30 * 60))
-  ) %>%
-  left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID") %>%
-  distinct(its_path, .keep_all = TRUE) 
-write.csv(LENA_counts, "data/LENA/Automated/LENA_counts.csv")
-
 ## Load in transcripts and automated metrics
 
-###read in data that has been mass exported via ELAN; add informative column names
+LENA_counts <-
+  read_csv("../data/LENA/Automated/VIHI_its_details.csv") %>%
+  filter(match_type == "VI" | VIHI_ID %in% TD_matches) %>% # only look at data from VI kids or their TD matches
+  mutate(
+    AWC_stand = AWC / (total_time_dur / (30 * 60)), # change AWC to be per 30 minutes (so that it aligns with the length of time our random sample annotations span)
+    CTC_stand = CTC / (total_time_dur / (30 * 60)) # change CTC to be per 30 minutes (so that it aligns with the length of time our random sample annotations span)
+  ) %>%
+  left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID") %>% # add a column that indicates which pair each child is a member of
+  distinct(its_path, .keep_all = TRUE) # remove duplicate rows
+write_csv(LENA_counts, "../data/LENA/Automated/LENA_counts.csv")
+
+
+###read in data that has been mass exported via ELAN
 
 VITD_transcripts <-
-  read.csv("data/LENA/Transcripts/Raw/VI_LENA_and_TD_matches_2023-09-11.csv") %>% 
+  read_csv("../data/LENA/Transcripts/Raw/VI_LENA_and_TD_matches_2023-09-11.csv") %>% 
   mutate(VIHI_ID = as.factor(str_sub(VIHI_ID, 1, 10))) %>%
-  filter(VIHI_ID %in% TD_matches | group == "VI") %>%
+  filter(VIHI_ID %in% TD_matches | group == "VI") %>% # only look at data from VI kids or their TD matches
   mutate(
-    utterance_clean = str_replace_all(utterance, "<\\w+>", ""),
-    utterance_clean = str_replace_all(utterance_clean, "=!\\w+\\s*", ""),
-    utterance_clean = str_replace_all(utterance_clean, "[[:punct:]&&[^']]", "")
+    utterance_clean = str_replace_all(utterance, "<\\w+>", ""), # Replace any substrings enclosed within angle brackets with an empty string (any of the slang)
+    utterance_clean = str_replace_all(utterance_clean, "=!\\w+\\s*", ""), # Remove any substrings starting with "=!" followed by one or more word characters and optional whitespace (get rid of the style markers =!shrieks)
+    utterance_clean = str_replace_all(utterance_clean, "[[:punct:]&&[^']]", "") # Remove any punctuation, except for apostrophes
   ) %>%
   filter(speaker != "EE1", speaker != "CHI") %>% # remove CHI utts and electronic noise
   filter(!utterance == c("xxx.")) %>% #remove unintelligible utterances
   filter(!grepl("utt@|inq@", speaker)) %>% # remove non-utterance info
-  mutate(utt_num = 1:nrow(.))
-
-write.csv(VITD_transcripts, "data/LENA/Transcripts/Derived/VITD_transcripts.csv")
+  mutate(utt_num = 1:nrow(.)) # number the utterances
+write_csv(VITD_transcripts, "../data/LENA/Transcripts/Derived/VITD_transcripts.csv")
 
 VITD_LENA_utterances_split <- VITD_transcripts %>%
   separate(utterance_clean,
@@ -97,41 +92,40 @@ VITD_LENA_words <- VITD_LENA_utterances_split %>%
     speaker
   ) %>% #remove unwanted columns
   filter(group != "HI" & speaker != "CHI")
-write.csv(VITD_LENA_words, "data/LENA/Transcripts/Derived/VITD_LENA_words.csv")
+write_csv(VITD_LENA_words, "../data/LENA/Transcripts/Derived/VITD_LENA_words.csv")
 
 # quantity ----
 
 # count word tokens in annotations
 manual_word_tokens <- VITD_LENA_words %>%
-  filter(sampling_type == "random") %>%
+  filter(sampling_type == "random") %>% # for quantity measures, we're only looking at random samples (high-volume might overrepresent)
   group_by(VIHI_ID) %>%
-  summarise(tokens = n()) %>%
-  left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID") %>%
-  mutate(group = as.factor(str_sub(VIHI_ID, 1, 2))) %>%
+  summarise(tokens = n()) %>% # count the number of rows, grouped by child (VIHI_ID). each row is one word, so this should give us the token count.
+  left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID") %>% # add pair info
+  mutate(group = as.factor(str_sub(VIHI_ID, 1, 2))) %>% # add group (determined by VIHI_ID)
   distinct(VIHI_ID, .keep_all = TRUE) 
 
-write.csv(manual_word_tokens,
-          "data/LENA/Transcripts/Derived/manual_word_tokens.csv")
+write_csv(manual_word_tokens,
+          "../data/LENA/Transcripts/Derived/manual_word_tokens.csv")
 
-# interactiveness quality ----
+# interactiveness  ----
 ## CTC----
 ## child-directed----
 xds_props_wide <- VITD_transcripts %>%
-  filter(sampling_type == "random") %>%
+  filter(sampling_type == "random") %>% # for interaction measures, we're only looking at random samples (high-volume might overrepresent)
   group_by(group, VIHI_ID) %>%
-  summarise(
-    total = n(),
+  summarise( #calculate proportions by xds. erin2zhenya: it seems like there should be a less redundant way to do this
+    total = n(), 
     prop_ADS = sum(xds == "A") / total,
     prop_CDS = sum(xds == "C") / total,
     prop_BDS = sum(xds == "B") / total,
     prop_TDS = sum(xds == "T") / total,
     prop_ODS = sum(xds == "O") / total,
     prop_UDS = sum(xds == "U") / total,
-    prop_PDS = sum(xds == "P") / total
+    prop_PDS = sum(xds == "P") / total 
   ) %>%
   left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID")
-write.csv(xds_props_wide,
-          "data/LENA/Transcripts/Derived/xds_props_wide.csv")
+write_csv(xds_props_wide, "../data/LENA/Transcripts/Derived/xds_props_wide.csv")
 xds_props <- xds_props_wide %>%
   pivot_longer(
     cols = prop_ADS:prop_PDS,
@@ -139,7 +133,7 @@ xds_props <- xds_props_wide %>%
     names_prefix = "prop_",
     values_to = "prop"
   )
-write.csv(xds_props, "data/LENA/Transcripts/Derived/xds_props.csv")
+write_csv(xds_props, "../data/LENA/Transcripts/Derived/xds_props.csv")
 
 # linguistic quality ----
 ## TTR ----
@@ -163,9 +157,9 @@ TTR_calculations <- VITD_LENA_words %>%
             mean_ttr = mean(ttr)) %>%
   mutate(group = as.factor(str_sub(VIHI_ID, 1, 2))) %>%
   left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID")
-write.csv(TTR_calculations, "data/LENA/Transcripts/Derived/TTR_calculations.csv")
+write_csv(TTR_calculations, "../data/LENA/Transcripts/Derived/TTR_calculations.csv")
 
-## MLU ----
+## MLU ---- erin2zhenya: I don't like how I calculated MLU. Feels sloppy and open to errors, but I struggled with others. Do you have ideas?
 tokenized_VITD_transcripts <-
   morphemepiece_tokenize(
     VITD_transcripts$utterance_clean,
@@ -174,17 +168,17 @@ tokenized_VITD_transcripts <-
     unk_token = "[UNK]",
     max_chars = 100
   ) %>% #split utterances into morphemes (based on entries in the morphemepiece "dictionary"). each listing has its own number
-  plyr::ldply(rbind) %>% # split each tokenized_VI, then bind each as rows in a dataframe
+  plyr::ldply(rbind) %>% # split each tokenized_VI, then bind each as rows in a dataframe 
   mutate_all(funs(ifelse(is.na(.), 0, 1)))
 
 tokenized_VITD_transcripts_with_counts <-
   tokenized_VITD_transcripts %>%
-  mutate(morphemecount = rowSums(tokenized_VITD_transcripts)) %>%
-  mutate(utt_num = 1:nrow(tokenized_VITD_transcripts))
+  mutate(morphemecount = rowSums(tokenized_VITD_transcripts)) %>% #add up the number of morphemes in each utterance
+  mutate(utt_num = 1:nrow(tokenized_VITD_transcripts)) # add utterance number back in. erin2zhenya: THIS STEP MAKES ME NERVOUS. it looks like it works properly (based on me checking it manually), but I don't feel like we have any great way to verify that the utt_num generated here perfectly matches with the utt_num generated above.
 
 simple_morpheme_counts <-
   tokenized_VITD_transcripts_with_counts %>%
-  left_join((VITD_transcripts %>% dplyr::select(-group,-code,-con))) %>%
+  left_join((VITD_transcripts %>% dplyr::select(-group,-code,-con))) %>% 
   dplyr::select(VIHI_ID, utt_num, speaker, xds, utterance_clean, morphemecount) %>%
   mutate(group = as.factor(str_sub(VIHI_ID, 1, 2)))
 
@@ -193,12 +187,13 @@ MLUs <- simple_morpheme_counts %>%
   group_by(group, VIHI_ID) %>%
   summarise(MLU = mean(morphemecount)) %>%
   left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID")
-write.csv(MLUs, "data/LENA/Transcripts/Derived/MLUs.csv")
+write_csv(MLUs, "../data/LENA/Transcripts/Derived/MLUs.csv")
 
+#### get a random subset of utterances, and write out to a csv. DO NOT UNCOMMENT CODE AND RERUN THESE STEPS (risks overwriting previous csv)
 # random_MLU_subset <- simple_morpheme_counts%>%
 #     filter(!is.na(morphemecount) &!is.na(utterance)) %>%
 #   sample_n(round(nrow(simple_morpheme_counts%>%filter(!is.na(morphemecount) &!is.na(utterance)))*.1))
-
+#### hide identifying information about participants
 # secret_random_MLU_subset <- random_MLU_subset %>%
 #   dplyr::select(utt_num, speaker, xds,utterance) %>%
 #   mutate(manual_morpheme_count = "NA")
@@ -206,9 +201,9 @@ write.csv(MLUs, "data/LENA/Transcripts/Derived/MLUs.csv")
 # write_csv(secret_random_MLU_subset, "data/LENA/Transcripts/Derived/secret_random_MLU_subset.csv")
 
 random_MLU_subset <-
-  read.csv("data/LENA/Transcripts/Derived/random_MLU_subset.csv")
-manually_coded_MLU_subset <-
-  read.csv("data/LENA/Transcripts/Derived/manually_coded_MLU_subset.csv")
+  read_csv("../data/LENA/Transcripts/Derived/random_MLU_subset.csv")
+manually_coded_MLU_subset <- #after manually counting morphemes (done by EC & LR), this gets read back in for validity
+  read_csv("../data/LENA/Transcripts/Derived/manually_coded_MLU_subset.csv")
 
 
 # Calculate agreement between the manual coding and original data
@@ -225,17 +220,17 @@ MLU_subset_for_agreement <-
     morpheme_diff = morphemecount - manual_morpheme_count,
     abs_morpheme_diff = abs(morphemecount - manual_morpheme_count)
   )
-write.csv(MLU_subset_for_agreement,"data/LENA/Transcripts/Derived/MLU_subset_for_agreement.csv")
+write_csv(MLU_subset_for_agreement,"../data/LENA/Transcripts/Derived/MLU_subset_for_agreement.csv")
 
 
 
 # conceptual quality ----
 ## sensory word props ----
 sensory_props_wide <- VITD_LENA_words %>%
-  anti_join(stop_words, by = c("Word" = "word")) %>%
+  anti_join(stop_words, by = c("Word" = "word")) %>% # get rid of stop words
   filter(!is.na(Auditory.mean))  %>% # filter out words that don't have a perceptual rating
   mutate(
-    Modality = case_when(
+    Modality = case_when( # create extra columns for Amodal and Multimodal. 
       Max_strength.perceptual < 3.5 ~ "Amodal",
       Exclusivity.perceptual <= .5 ~ "Multimodal",
       Max_strength.perceptual > 4  & Exclusivity.perceptual >= .3 ~ Dominant.perceptual,
@@ -245,7 +240,7 @@ sensory_props_wide <- VITD_LENA_words %>%
   ) %>%
   group_by(group, VIHI_ID) %>%
   summarise(
-    total = n(),
+    total = n(), # calculate proportions of words by modality out of total word tokens
     prop_Auditory = sum(Modality == "Auditory") / total,
     prop_Visual = sum(Modality == "Visual") / total,
     prop_Olfactory = sum(Modality == "Olfactory") / total,
@@ -256,8 +251,8 @@ sensory_props_wide <- VITD_LENA_words %>%
     prop_Amodal = sum(Modality == "Amodal") / total
   ) %>%
   left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID")
-write.csv(sensory_props_wide,
-          "data/LENA/Transcripts/Derived/sensory_props_wide.csv")
+write_csv(sensory_props_wide,
+          "../data/LENA/Transcripts/Derived/sensory_props_wide.csv")
 sensory_props <- sensory_props_wide %>%
   pivot_longer(
     cols = prop_Auditory:prop_Amodal,
@@ -266,29 +261,29 @@ sensory_props <- sensory_props_wide %>%
     values_to = "prop"
   )
 
-write.csv(sensory_props,
-          "data/LENA/Transcripts/Derived/sensory_props.csv")
+write_csv(sensory_props,
+          "../data/LENA/Transcripts/Derived/sensory_props.csv")
 
-## tense/displacement----
-udpipe_english <- udpipe_download_model(language = "english")
+## tense/displacement---- erin2zhenya: take an extra hard look at this? I want to be super sure that this is working how we think it's working
+udpipe_english <- udpipe_download_model(language = "english") # download the udpipe english model
 udmodel_english <-
   udpipe_load_model(file = udpipe_english$file_model)
-annotated_utterances <- udpipe_annotate(udmodel_english,
+annotated_utterances <- udpipe_annotate(udmodel_english, # apply the udpipe model of english to the cleaned utterances. this should give us syntactical parsing (not perfect, but as we see later down, pretty similar to human raters)
                                         x = VITD_transcripts$utterance_clean,
                                         doc_id = VITD_transcripts$VIHI_ID) %>%
   as.data.frame()
-write.csv(annotated_utterances,
-          "data/LENA/Transcripts/Derived/annotated_utterances.csv")
+write_csv(annotated_utterances,
+          "../data/LENA/Transcripts/Derived/annotated_utterances.csv")
 
 verbs_only <- annotated_utterances %>%
   filter(
-    xpos %in% c("VB", "VBD", "VBP", "VBN", "VBG", "VBZ", "AUX", "MD"),
+    xpos %in% c("VB", "VBD", "VBP", "VBN", "VBG", "VBZ", "AUX", "MD"), # filter to verbs only
     token != "=!" &
       token != "xxx"
   ) %>%
   distinct(doc_id, paragraph_id, sentence_id, .keep_all = TRUE) %>%
   mutate(
-    temporality = case_when(
+    temporality = case_when( # this is based on EC & LR's top-down judgments of how to categorize words based on tense
       grepl('Tense=Past', feats) ~ "displaced",
       grepl('Mood=Imp', feats) ~ "uncategorized",
       xpos == "MD" ~ "displaced",
@@ -305,9 +300,11 @@ verbs_only <- annotated_utterances %>%
       TRUE ~ "uncategorized"
     )
   )
-write.csv(verbs_only, "data/LENA/Transcripts/Derived/verbs_only.csv")
+write_csv(verbs_only, "../data/LENA/Transcripts/Derived/verbs_only.csv")
 
-# Randomly select a subset of the utterances for manual coding
+
+# Randomly select a subset of the utterances for manual coding DO NOT UNCOMMENT CODE AND RERUN THESE STEPS (risks overwriting previous csv)
+#### this follows the same procedure as above ^ 
 # random_displacement_subset <- verbs_only %>%
 #   sample_n(round((0.1 * nrow(verbs_only))))
 # secret_random_displacement_subset <- random_displacement_subset %>%
@@ -317,10 +314,10 @@ write.csv(verbs_only, "data/LENA/Transcripts/Derived/verbs_only.csv")
 # write_csv(secret_random_displacement_subset, "data/LENA/Transcripts/Derived/secret_random_displacement_subset.csv")
 # Once manual coding is done in Excel, read the manually coded CSV file back into R
 random_displacement_subset <-
-  read_csv("data/LENA/Transcripts/Derived/random_displacement_subset.csv")
+  read_csv("../data/LENA/Transcripts/Derived/random_displacement_subset.csv")
 
-manually_coded_displacement_subset <-
-  read_csv("data/LENA/Transcripts/Derived/manually_coded_displacement_subset.csv")
+manually_coded_displacement_subset <- # after manual utterance tagging by EC, LR, and GL, this gets read back in for validity
+  read_csv("../data/LENA/Transcripts/Derived/manually_coded_displacement_subset.csv")
 # Calculate agreement between the manual coding and original data
 displacement_subset_for_agreement <-
   left_join(
@@ -347,7 +344,7 @@ displacement_subset_for_agreement <-
                 manual_temporality, 
                 feats) %>%
   filter(!grepl("Mood=Imp", feats))
-write.csv(displacement_subset_for_agreement, "data/LENA/Transcripts/Derived/displacement_subset_for_agreement.csv")
+write_csv(displacement_subset_for_agreement, "../data/LENA/Transcripts/Derived/displacement_subset_for_agreement.csv")
 
 # Calculate Percent Agreement
 displacement_agreement <-
@@ -368,7 +365,7 @@ temporality_props_wide <- verbs_only %>%
   dplyr::rename(VIHI_ID = doc_id) %>%
   group_by(VIHI_ID) %>%
   summarize(
-    verb_utt_count = n(),
+    verb_utt_count = n(), # calculate proportions by verb tense out of total utterances
     prop_displaced = (sum(temporality == "displaced") / verb_utt_count),
     prop_present =  (sum(temporality == "present") / verb_utt_count),
     prop_uncategorized = (sum(temporality == "uncategorized") /
@@ -376,9 +373,9 @@ temporality_props_wide <- verbs_only %>%
   ) %>%
   mutate(group = as.factor(str_sub(VIHI_ID, 1, 2))) %>%
   left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID")
-write.csv(
+write_csv(
   temporality_props_wide,
-  "data/LENA/Transcripts/Derived/temporality_props_wide.csv"
+  "../data/LENA/Transcripts/Derived/temporality_props_wide.csv"
 )
 temporality_props <- temporality_props_wide %>%
   pivot_longer(
@@ -387,23 +384,21 @@ temporality_props <- temporality_props_wide %>%
     names_prefix = "prop_",
     values_to = "prop"
   )
-write.csv(temporality_props,
-          "data/LENA/Transcripts/Derived/temporality_props.csv")
+write_csv(temporality_props,
+          "../data/LENA/Transcripts/Derived/temporality_props.csv")
 
-## content word CBOI ----
-content_words_only <- annotated_utterances %>%
-  filter(upos %in% c("ADJ", "ADV", "NOUN", "VERB")) %>%
-  left_join(CBOI_norms, by = c("lemma" = "Word")) %>%
-  filter(CBOI_Mean != "NA") %>%
-  dplyr::rename(VIHI_ID = doc_id) %>%
-  dplyr::select(VIHI_ID, sentence, token, lemma, upos, CBOI_Mean) %>%
-  mutate(group = as.factor(str_sub(VIHI_ID, 1, 2)))
-write.csv(content_words_only,
-          "data/LENA/Transcripts/Derived/content_words_only.csv")
-subj_CBOI_means <- content_words_only %>%
-  group_by(group, VIHI_ID) %>%
-  summarise(CBOI_Mean = mean(CBOI_Mean)) %>%
-  left_join((VI_matches_demo %>% dplyr::select(VIHI_ID, pair)), by = "VIHI_ID")
-write.csv(subj_CBOI_means,
-          "data/LENA/Transcripts/Derived/subj_CBOI_means.csv")
 
+
+
+
+# join all input variables into one big dataframe
+lotta_data <- LENA_counts %>%
+  left_join(MLUs, by=c("VIHI_ID", "group"))  %>%
+  left_join(TTR_calculations, by=c("VIHI_ID", "group")) %>%
+  left_join(xds_props_wide %>% dplyr::select(-total), by=c("VIHI_ID", "group")) %>%
+  left_join(sensory_props_wide %>% dplyr::select(-total), by=c("VIHI_ID", "group"))%>%
+  left_join(temporality_props_wide, by=c("VIHI_ID", "group")) %>%
+  mutate(ParticipantNumber = as.factor(str_sub(VIHI_ID, 1, 6))) %>%
+  mutate(LENA_age_in_days = as.numeric(str_sub(VIHI_ID, 8, length(VIHI_ID))))%>%
+  distinct(ParticipantNumber, .keep_all=TRUE)
+write_csv(lotta_data, "../data/LENA/Transcripts/Derived/lotta_data.csv")
