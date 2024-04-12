01fef1ad39650a000bf14052fa992806bbb5c060
erinecampbell <eec35@duke.edu>
2024-04-12 14:43:54 -0400

    implementing the rest of eb and zk's feedback!!!

--- manuscript/input_quality_preprocessing.R
+++ manuscript/input_quality_preprocessing.R
@@ -2,8 +2,6 @@
 
 # praise: zhenya2erin: While this code is less clean than the code in the manuscript, I still didn't need any help figuring out what happens here. So most of my comments below are about implementation details.
 
-# issue: zhenya2erin: Distinct issue. Using `distinct(***, .keep_all=TRUE)` is a hard-to-notice error waiting to happen because you are silently dropping rows with potentially non-redundant information. It groups the dataframe by the variables in `***` and keeps the first row in each group. Even when that is exactly what one wants (like when picking the first verb for temporality judgment), the table should be sorted within groups first so that "the first" makes sense. And if you do that, you might as well use `filter(row_number() == 1` or `slice(1)` instead of `distinct(***, .keep_all=TRUE)`. In all other cases, the duplication should be investigated and avoided.
-
 library(dplyr)
 library(readr)
 library(stringr)
@@ -15,7 +13,8 @@ library(udpipe)
 # functions
 get_match_number <- function(df) { 
   df %>%
-    left_join((VI_matches_demo %>% select(VIHI_ID,pair)), by = "VIHI_ID")
+    left_join((VI_matches_demo %>% select(VIHI_ID,pair)), by = "VIHI_ID") %>%
+    distinct()
 }
 
 ## demographics
@@ -51,12 +50,11 @@ LENA_counts <-
   read_csv("../data/LENA/Automated/VIHI_its_details.csv") %>%
   filter(match_type == "VI" | VIHI_ID %in% TD_matches) %>% # only look at data from VI kids or their TD matches
   mutate(
-    AWC_stand = AWC / (total_time_dur / (30 * 60)), # change AWC to be per 30 minutes (so that it aligns with the length of time our random sample annotations span)
-    CTC_stand = CTC / (total_time_dur / (30 * 60)) # change CTC to be per 30 minutes (so that it aligns with the length of time our random sample annotations span)
+    AWC_per_hour = AWC / (total_time_dur / 3600), # change AWC to be per hour 
+    CTC_per_hour = CTC / (total_time_dur / 3600) # change CTC to be per hour
   ) %>%
   get_match_number() %>% # add a column that indicates which pair each child is a member of
-  # issue: zhenya2erin: See the "Distinct issue" at the top of the script.
-  distinct(its_path, .keep_all = TRUE) # remove duplicate rows
+  distinct(its_path, .keep_all = TRUE)
 write_csv(LENA_counts, "../data/LENA/Automated/LENA_counts.csv")
 
 
@@ -127,11 +125,11 @@ write_csv(VITD_LENA_words, "../data/LENA/Transcripts/Derived/VITD_LENA_words.csv
 manual_word_tokens <- VITD_LENA_words %>%
   filter(sampling_type == "random") %>% # for quantity measures, we're only looking at random samples (high-volume might overrepresent)
   group_by(VIHI_ID) %>%
-  summarise(tokens = n()) %>% # count the number of rows, grouped by child (VIHI_ID). each row is one word, so this should give us the token count.
+  summarise(tokens = n(),# count the number of rows, grouped by child (VIHI_ID). each row is one word, so this should give us the token count.
+            MWC_per_hour = tokens*2) %>% #since this is over 30 minutes of annotations, we multiply by 2 to get the words/hour
+  select(VIHI_ID, tokens, MWC_per_hour) %>%
   get_match_number() %>% # add pair info
-  mutate(group = as.factor(str_sub(VIHI_ID, 1, 2))) %>% # add group (determined by VIHI_ID)
-  # issue: zhenya2erin: See the "Distinct issue" at the top of the script.
-  distinct(VIHI_ID, .keep_all = TRUE) 
+  mutate(group = as.factor(str_sub(VIHI_ID, 1, 2))) # add group (determined by VIHI_ID)
 
 write_csv(manual_word_tokens,
           "../data/LENA/Transcripts/Derived/manual_word_tokens.csv")
@@ -166,7 +164,6 @@ write_csv(xds_props, "../data/LENA/Transcripts/Derived/xds_props.csv")
 # linguistic quality ----
 ## TTR ----
 ### calculate type-token ratio in annotations
-# issue: zhenya2erin: 
 TTR_calculations <- VITD_LENA_words %>%
   filter(Word != "0",
          speaker != "CHI",
@@ -221,8 +218,7 @@ manually_coded_MLU_subset <- #after manually counting morphemes (done by EC & LR
 # Calculate agreement between the manual coding and original data
 MLU_subset_for_agreement <-
   left_join((
-    # issue: zhenya2erin: See the "Distinct issue" at the top of the script.
-    manually_coded_MLU_subset %>% distinct(utterance, utt_num, .keep_all = TRUE)
+    manually_coded_MLU_subset 
   ),
   random_MLU_subset,
   by = c("utterance", "utt_num")
@@ -277,59 +273,33 @@ sensory_props <- sensory_props_wide %>%
 write_csv(sensory_props,
           "../data/LENA/Transcripts/Derived/sensory_props.csv")
 
-## tense/displacement---- erin2zhenya: take an extra hard look at this? I want to be super sure that this is working how we think it's working
-# zhenya2erin: I'll describe how *I* think it is working and how that might differ from what I understood from the manuscript.
-#
-# annotated_utterances:
-# - We split each utterance into rows with one word token per row and add tags to each token (e.g., part of speech, lemma, etc.).
-# - For each row/token, we retain the information about the recording it came from and the text of the utterance it came from. However, we lose the information about the id of the utterance it came from though it should be identical to paragraph_id so probably not a big deal.
-# - Each token is additionally assigned a sentence_id based on how `udpipe` split the utterance into sentences.
-#
-# verbs_only:
-# - We remove tokens that are not verbs or auxiliaries based on their xpos value implicitly excluding `xpos == 'NNS'` by not including it in the list of tags to keep. This implicitly removes all utterances that don't include such tokens at all and removes some of the sentences from further utterances.
-# - We remove "=!" and "xxx" tokens not trusting features inferred by udpipe for these tokens when they are tagged as verbs. This also implicitly removes whole utterances and individual sentences.
-# - From each of the remaining sentences (not utterances!), we keep only the first verb as identified by the conditions above (xpos in a special list, not "=!" or "xxx").
-# - Finally, we assign "temporality" based on the features extracted by `udpipe` and the sentence including certain word combinations.
-#
-# While the above is done in two steps, it could be done in one because we don't seem to use `annotated_utterances` anywhere.
-#
-# And here is what I understood from the manuscript:
-# - Each utterance is categorized as "displaced" or "present" or not categorized. This is done based on the first verb in the utterance. It is unclear how utterances without verbs are treated.
-# - The number of uncategorized utterances and the total number of utterances are reported.
-#
-# Here is how my understanding of code differs from my understanding of the manuscript:
-# - The code drops utterances without verbs and so the reported denominator is the number of utterances (really, sentences) with verbs, not the total number of utterances in the corpus and the reported numerator is the number of utterances (really, sentences) with verbs that are categorized as "displaced" or "present".
-# - The code's output contains sentence-level information and not the utterance-level and not the utterance-level information as it says in the manuscript.
-#
-# I believe that the code, the manuscript, or both should be updated to make the two consistent with each other.
-#
-# I've also added some comments to the code below.
+content_words_only <- VITD_LENA_words %>%
+  anti_join(tidytext::stop_words, by = c("Word" = "word")) # get rid of stop words
+
+percentage_na_sensory <- (sum(is.na(VITD_LENA_words$Auditory.mean)) / nrow(VITD_LENA_words)) * 100
+percentage_not_na_sensory <- round(100 - percentage_na_sensory)
+
+write_rds(percentage_not_na_sensory, "../data/LENA/Transcripts/Derived/percentage_not_na_sensory.rds")
+
+## tense/displacement---- 
 
 udpipe_english <- udpipe_download_model(language = "english") # download the udpipe english model
 udmodel_english <-
   udpipe_load_model(file = udpipe_english$file_model)
-# issue: zhenya2erin: I think it would be cleaner to use a compbination of VIHI_ID and utt_num as the doc_id. This would, for example, allow us to left-join temporalities to the utterances. Probably paragraph_id is identical to utt_num but why risk it?
-annotated_utterances <- udpipe_annotate(udmodel_english, # apply the udpipe model of english to the cleaned utterances. this should give us syntactical parsing (not perfect, but as we see later down, pretty similar to human raters)
+verbs_only <- udpipe_annotate(udmodel_english, # apply the udpipe model of english to the cleaned utterances. this should give us syntactical parsing (not perfect, but as we see later down, pretty similar to human raters)
                                         x = VITD_transcripts$utterance_clean,
                                         doc_id = VITD_transcripts$VIHI_ID) %>%
-  as.data.frame()
-write_csv(annotated_utterances,
-          "../data/LENA/Transcripts/Derived/annotated_utterances.csv")
-
-verbs_only <- annotated_utterances %>%
-  # issue: zhenya2erin: Why is the filtering done on xpos instead of upos? As far as I could tell, the only difference is that the current version doesn't count tokens with upos == 'VERB' *and* xpos == 'NNS' as verbs. Is that intentional? I would either switch to filtering based on upos or explain why the NNS tokens are excluded
+  as.data.frame() %>%
   filter(
     xpos %in% c("VB", "VBD", "VBP", "VBN", "VBG", "VBZ", "AUX", "MD"), # filter to verbs only
     token != "=!" &
       token != "xxx"
   ) %>%
-  # suggestion: zhenya2erin: In this case, `distinct` keeping the first row is the point and since the order has most likely been maintained, it should work as expected. For clarity, however, I would explicitly sort verbs within each utterance by `sentence_id` and `token_id` and then select the first row (`filter(row_number() == 1)` or `slice(1)`).
-  # issue: zhenya2erin: I think parsing of the utterances into sentences is of no interest to us. Unless I am worng about that, I would drop `sentence_id` from the next line.
   distinct(doc_id, paragraph_id, sentence_id, .keep_all = TRUE) %>%
   mutate(
     temporality = case_when( # this is based on EC & LR's top-down judgments of how to categorize words based on tense
       grepl('Tense=Past', feats) ~ "displaced",
-      grepl('Mood=Imp', feats) ~ "uncategorized",
+      grepl('Mood=Imp', feats) ~ "ambiguous",
       xpos == "MD" ~ "displaced",
       grepl('gonna', sentence) |
         grepl('gotta', sentence) |
@@ -337,12 +307,11 @@ verbs_only <- annotated_utterances %>%
         grepl('going to', sentence) |
         grepl('got to', sentence) |
         grepl('want to', sentence) |
-        # note: zhenya2erin: I don't know if aperitifs and digestifs form the leitmotif of the corpus but grepl('if ') would match the singular forms of all three of these nouns :-)
-        grepl('have to', sentence) | grepl('if ', sentence) ~ "displaced",
+        grepl('have to', sentence) | grepl("\\bif\\b", sentence) ~ "displaced",
       grepl('Mood=Ind', feats) &
         grepl('Tense=Pres', feats) |
         grepl('VerbForm=Ger', feats) ~ "present",
-      TRUE ~ "uncategorized"
+      TRUE ~ "ambiguous"
     )
   )
 write_csv(verbs_only, "../data/LENA/Transcripts/Derived/verbs_only.csv")
@@ -413,7 +382,7 @@ temporality_props_wide <- verbs_only %>%
     verb_utt_count = n(), # calculate proportions by verb tense out of total utterances
     prop_displaced = (sum(temporality == "displaced") / verb_utt_count),
     prop_present =  (sum(temporality == "present") / verb_utt_count),
-    prop_uncategorized = (sum(temporality == "uncategorized") /
+    prop_ambiguous = (sum(temporality == "ambiguous") /
                             verb_utt_count)
   ) %>%
   mutate(group = as.factor(str_sub(VIHI_ID, 1, 2))) %>%
@@ -424,31 +393,13 @@ write_csv(
 )
 temporality_props <- temporality_props_wide %>%
   pivot_longer(
-    cols = prop_displaced:prop_uncategorized,
+    cols = prop_displaced:prop_ambiguous,
     names_to = "verb_temporality",
     names_prefix = "prop_",
     values_to = "prop"
   )
 write_csv(temporality_props,
           "../data/LENA/Transcripts/Derived/temporality_props.csv")
-
-
-
-
-
-# join all input variables into one big dataframe
-lotta_data <- LENA_counts %>%
-  left_join(MLUs, by=c("VIHI_ID", "group"))  %>%
-  left_join(TTR_calculations, by=c("VIHI_ID", "group")) %>%
-  left_join(xds_props_wide %>% dplyr::select(-total), by=c("VIHI_ID", "group")) %>%
-  left_join(sensory_props_wide %>% dplyr::select(-total), by=c("VIHI_ID", "group"))%>%
-  left_join(temporality_props_wide, by=c("VIHI_ID", "group")) %>%
-  mutate(ParticipantNumber = as.factor(str_sub(VIHI_ID, 1, 6))) %>%
-  mutate(LENA_age_in_days = as.numeric(str_sub(VIHI_ID, 8, length(VIHI_ID))))%>%
-  # issue: zhenya2erin: See the "Distinct issue" at the top of the script.
-  distinct(ParticipantNumber, .keep_all=TRUE)
-write_csv(lotta_data, "../data/LENA/Transcripts/Derived/lotta_data.csv")
-
 
 
 perc_random_silent <-
